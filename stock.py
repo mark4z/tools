151367c0404a1a2d3bbf68d141798bdc198b0f62
mengchao.lv <anymre@outlook.com>
2021-11-05 01:11:16 +0800

    test 刷新

--- stock.py
+++ stock.py
@@ -9,6 +9,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.touch_actions import TouchActions
 from selenium.webdriver.support import wait
 
+Local = True
+
+Width = 287
+
 
 def send(driver, cmd, params=None):
     if params is None:
@@ -23,48 +27,67 @@ def send(driver, cmd, params=None):
 
 
 def process(url_str, options):
-    with webdriver.Remote(
-            options=options,
-            command_executor='http://127.0.0.1:4444/wd/hub'
-    ) as driver:
-    # with webdriver.Chrome(
-    #         options=options
-    # ) as driver:
-        print(send(driver, 'Emulation.setNavigatorOverrides', {"platform": "iPhone"}))
-        # driver.execute_cdp_cmd('Emulation.setNavigatorOverrides', {"platform": "iPhone"})
+    if Local:
+        with webdriver.Chrome(
+                options=options
+        ) as driver:
 
-        driver.set_window_size(376, 812)
-        driver.get(url_str)
+            for ii in range(0, 2):
+                print(ii)
+                getAndPerform(driver, url_str)
 
-        print('scroll')
-        for i in range(0, (int(round(time.time() * 1000))) % 5):
-            TouchActions(driver).scroll(150, 200).perform()
-        print('scroll end')
 
-        images = driver.find_elements(by=By.TAG_NAME, value="img")
-        print(images)
+    else:
+        with webdriver.Remote(
+                options=options,
+                command_executor='http://127.0.0.1:4444/wd/hub'
+        ) as driver:
+            getAndPerform(driver, url_str)
 
-        for i in range(0, len(images)):
-            try:
-                ActionChains(driver).click()
-                print('pass')
-                break
-            except AttributeError as err:
-                print(err)
-                pass
-        driver.save_screenshot("./" + str(int(round(time.time() * 1000))) + ".png")
-        time.sleep(10)
+
+def getAndPerform(driver, url_str):
+    # UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) ' \
+    #      'Mobile/15E148 MicroMessenger/8.0.15(0x18000f24) NetType/WIFI Language/zh_CN '
+    # print(driver.execute_cdp_cmd('Emulation.setUserAgentOverride', {"userAgent": UA, "platform": "iPhone"}))
+    #
+    # global Width
+    # Width = Width + 1
+    # print(driver.execute_cdp_cmd('Emulation.setDeviceMetricsOverride',
+    #                              {"width": 378, "height": Width, "deviceScaleFactor": 2.0, "mobile": True}))
+
+    driver.set_window_size(376, 812)
+
+    # driver.switch_to.new_window('tab')
+
+    driver.get(url_str)
+    print('scroll')
+    # for i in range(0, (int(round(time.time() * 1000))) % 5):
+    #     TouchActions(driver).scroll(150, 200).perform()
+    print('scroll end')
+    images = driver.find_elements(by=By.TAG_NAME, value="img")
+    print(images)
+    for i in range(0, len(images)):
+        try:
+            ActionChains(driver).click()
+            print('pass')
+            break
+        except AttributeError as err:
+            print(err)
+            pass
+    driver.save_screenshot("./" + str(int(round(time.time() * 1000))) + ".png")
+    time.sleep(2)
+
+    driver.close()
 
 
 if __name__ == '__main__':
-    # url = "http://47.100.220.7/"
-    url = "https://bhjtomghwqdvm.kuaizhan.com/?sif=1e927p7ajg9ijg168q2ib2i969"
+    url = "http://rjhg.oradt.com.cn/1e927p7ajg9ijg168q2ib2i969"
     # url = "https://www.baidu.com"
 
     option = webdriver.ChromeOptions()
     option.add_argument('--disable-gpu')
     option.add_argument('--no-sandbox')
-    option.add_argument('--headless')
+    # option.add_argument('--headless')
 
     experimentalFlags = [
         "same-site-by-default-cookies@2",
@@ -92,6 +115,8 @@ if __name__ == '__main__':
             "sslProxy": PROXY,
             "proxyType": "MANUAL",
         })
-        process(url, option)
+        for j in range(0, 1):
+            process(url, option)
+            print(str(j) + "-------------------")
     now2 = (int(round(time.time() * 1000)))
     print(now2 - now)
